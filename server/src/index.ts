edee2b6f0faa3c704f1690f84ade64b1b574332b
MohammedIrfan244 <kuttappuirfan244@gmail.com>
2025-02-20 10:28:23 +0530

    fix : update log message concatenation for server port

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,5 +9,5 @@ app.use(cors());
 app.use(e.json());
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+    console.log("Server running on port " + process.env.PORT);
 });
