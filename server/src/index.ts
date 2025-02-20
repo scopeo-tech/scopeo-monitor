13269f826426ed858dbbbad2ff1e54dca93b05b4
navaf <navafsoft@gmail.com>
2025-02-20 13:26:43 +0530

    add : implement user authentication with registration and login functionality

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
 import e from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import dbConnect from "./config/dbConnect";
+
 
 const app = e();
 dotenv.config();
-
+dbConnect()
 app.use(cors());
 app.use(e.json());
 
