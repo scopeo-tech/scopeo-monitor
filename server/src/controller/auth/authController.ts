d56ac9e4cb620a1d446b07562d955d569ad3b92c
Navaf <navafsoft@gmail.com>
2025-02-20 21:11:24 +0530

    Navaf auth (#21)
    * add : user controller implementation
    
    * add : implement user authentication with registration and login functionality
    
    * fix: update user import path in authController and add CustomError utility
    
    * feat: implement user authentication with registration and login endpoints, add custom error handling
    
    * fix: enhance user login response with success message and token, improve code formatting
    
    * feat: add user logout and token refresh functionality, improve error handling in auth controller
    
    * feat: implement token verification middleware for authentication

--- server/src/controller/auth/authController.ts
+++ server/src/controller/auth/authController.ts
@@ -3,18 +3,19 @@ import User from "../../model/userModel";
 import { loginSchema, registerSchema } from "../../lib/bodyValidations/auth";
 import bcrypt from "bcryptjs"
 import { CustomError } from "../../lib/util/CustomError";
-import { createAccessToken } from "../../lib/jwt";
+import { createAccessToken, createRefreshToken } from "../../lib/jwt";
+import jwt from "jsonwebtoken";
 
 const userRegister = async (req: Request, res: Response , next: NextFunction)  => {
   const { username, email, password } = registerSchema.parse(req.body);
   const userNameExists = await User.findOne({ username });
   if (userNameExists){
-    throw new CustomError(400, "Username already exists");
+    return next(new CustomError(400, "Username already exists"));
   }  const emailExists = await User.findOne({ email });
   if (emailExists)
-    throw new CustomError(400, "Email already exists");
+    next(new CustomError(400, "Email already exists"));
   const hashedPassword = await bcrypt.hash(password, 12);
-  const user = await User.create({
+  await User.create({
     username,
     email,
     password: hashedPassword,
@@ -22,19 +23,60 @@ const userRegister = async (req: Request, res: Response , next: NextFunction)  =
   return res.status(201).json({ status : "success", message: "User created successfully", });
 };
 
-const userLogin = async (req: Request, res: Response) => {
+const userLogin = async (req: Request, res: Response, next : NextFunction) => {
   const { username, email, password } = loginSchema.parse(req.body);
   const user = await User.findOne({ $or: [{ username }, { email }] });
   if (!user){
-    throw new CustomError(400, "User not found");
+    return next(new CustomError(400, "User not found"));
   }
   const validPassword = await bcrypt.compare(password, user.password);
   if (!validPassword){
-    throw new CustomError(400, "Invalid password");
+    return next(new CustomError(400, "Invalid password"));
   }
   const token = createAccessToken(user._id.toString(), process.env.JWT_TOKEN as string);
-  return res.status(200).json({status: "success", message: "User logged successfully", user,token });
+  const refreshToken = createRefreshToken(user._id.toString(), process.env.JWT_REFRESH_TOKEN as string);
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  });
+
+  const currUser = {
+    _id: user._id,
+    username: user.username,
+    email: user.email,
+  };
+  return res.status(200).json({status: "success", message: "User logged successfully", user: currUser  ,token });
 };
 
-export { userRegister, userLogin };
+const userLogout = async (req: Request, res: Response) => {
+  res.clearCookie("refreshToken");
+  res.json({ message: "Logout successful" });
+};
+
+
+const refreshingToken = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const refreshToken = req.cookies?.refreshToken;
+    if (!refreshToken) {
+      return next(new CustomError(401, "No refresh token provided"));
+    }
+    const decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH_TOKEN as string) as { id: string };
+    if (!decoded || !decoded.id) {
+      return next(new CustomError(403, "Invalid refresh token"));
+    }
+    const accessToken = createAccessToken(decoded.id, process.env.JWT_TOKEN as string);
+    res.status(200).json({
+      status: "success",
+      message: "Token refreshed successfully",
+      token: accessToken,
+    });
+  } catch (error) {
+    return next(new CustomError(403, "Invalid or expired refresh token"));
+  }
+};
+
+
+export { userRegister, userLogin , userLogout , refreshingToken};
 
