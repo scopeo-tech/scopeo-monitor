79dbe5fb71bbaffc5106fdbc0e06a668765a688f
MohammedIrfan244 <kuttappuirfan244@gmail.com>
2025-02-20 16:02:59 +0530

    add : implement API key and pass key generation functions

---
+++ server/src/controller/project/projectController.ts
@@ -0,0 +1,23 @@
+import { Request, Response,NextFunction } from "express";
+import Project from "../../model/projectModel";
+import crypto from "crypto";
+
+const generateApiKey = () => {
+    return crypto.randomBytes(16).toString("hex");
+};
+const generatePassKey = () => {
+    return crypto.randomBytes(16).toString("hex");
+};
+
+const getApiKey = async (req: Request, res: Response, next: NextFunction) => {
+    const apiKey = generateApiKey();
+    return res.status(200).json({ status: "success", message: "API key generated", data:apiKey });
+};
+
+const getPassKey = async (req: Request, res: Response, next: NextFunction) => {
+    const passKey = generatePassKey();
+    return res.status(200).json({ status: "success", message: "Pass key generated", data:passKey });
+};
+
+
+export { getApiKey, getPassKey };
