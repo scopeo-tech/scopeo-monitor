2ab501f6757e5dd342ffd1127e4cc8d4f8b9f9c5
Navaf <navafsoft@gmail.com>
2025-02-20 16:45:52 +0530

    Navaf auth (#19)
    * add : user controller implementation
    
    * add : implement user authentication with registration and login functionality
    
    * fix: update user import path in authController and add CustomError utility
    
    * feat: implement user authentication with registration and login endpoints, add custom error handling
    
    * fix: enhance user login response with success message and token, improve code formatting

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,13 +7,16 @@ import authRouter from "./routes/authRoutes";
 
 const app = e();
 dotenv.config();
-dbConnect()
+dbConnect();
 app.use(cors());
 app.use(e.json());
 
-app.use(globalErrorHandler)
+
 app.use("/api/auth",authRouter) 
 
+
+app.use(globalErrorHandler)
+
 app.listen(process.env.PORT, () => {
-    console.log("Server running on port " + process.env.PORT);
+  console.log("Server running on port " + process.env.PORT);
 });
