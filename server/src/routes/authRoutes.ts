c4503b71ca3debb06d813e72ab31ce4a19ce58f1
navaf <navafsoft@gmail.com>
2025-02-20 21:08:58 +0530

    feat: add user logout and token refresh functionality, improve error handling in auth controller

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,11 +1,13 @@
 import express from 'express';
-import { userRegister, userLogin } from '../controller/auth/authController';
+import { userRegister, userLogin, userLogout, refreshingToken } from '../controller/auth/authController';
 import tryCatch from '../lib/util/tryCatch';
+import verifyToken  from '../middleware/verifyToken';
 
 const authRouter = express.Router();
 
 authRouter
 .post('/register', tryCatch(userRegister))
 .post('/login', tryCatch(userLogin))
-
+.post('/logout', tryCatch(userLogout))
+.post('/refresh-token', tryCatch(refreshingToken))
 export default authRouter
