28fab173ccaadc3131cecd53a6ce9a04a880b91c
sa7ik <mohammedsabikoo7@gmail.com>
2025-02-21 12:02:21 +0530

    add:getUserById

--- server/src/controller/user/userController.ts
+++ server/src/controller/user/userController.ts
@@ -4,11 +4,17 @@ import User from "../../model/userModel";
 import CustomError from "../../lib/util/CustomError";
 
 const getUserById = async (req:AuthenticatedRequest, res:Response, next:NextFunction) =>{
-    const user = await User.findById(req.user)
+    const user = await User.findById(req.user,{password:0})
     if (!user){
         return next (new CustomError(404,"user not found"))
     }
-    res.status(200).json({status:"success", message:"UserDetails", Data:user})
+    const Data = {
+        user : user._id,
+        username:user.username,
+        email:user.email,
+        joinedDate : user.createdAt 
+    }
+    res.status(200).json({status:"success", message:"UserDetails", Data})
 }
 
 export {getUserById}
