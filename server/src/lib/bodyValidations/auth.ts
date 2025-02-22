3f20a57ff1f4cda6195d23c920c4fa366bf12d35
navaf <navafsoft@gmail.com>
2025-02-22 09:58:37 +0530

    fix: update username validation to allow only lowercase letters, numbers, and underscores

--- server/src/lib/bodyValidations/auth.ts
+++ server/src/lib/bodyValidations/auth.ts
@@ -3,9 +3,9 @@ import z from "zod";
 export const registerSchema = z.object({
   email: z.string().email(),
   username: z.string().min(3).regex(
-      /^[a-zA-Z0-9_]+$/,
-      "Username can only contain letters, numbers and underscores"
-    ),
+    /^[a-z0-9_]+$/,
+    "Username can only contain lowercase letters, numbers, and underscores"
+  ),
   password: z.string().min(8, "Password must be at least 8 characters long"),
 });
 
