cfb8711509fc1473efb8c408668eec10e7a2dee7
sa7ik <mohammedsabikoo7@gmail.com>
2025-02-24 10:09:08 +0530

    add:userProjectCount

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getProjectList, getUserById } from '../controller/user/userController';
+import { getUserProjectCount, getProjectList, getUserById } from '../controller/user/userController';
 import tryCatch from '../lib/util/tryCatch';
 import verifyToken from '../middleware/verifyToken';
 
@@ -8,5 +8,6 @@ const userRoutes = express.Router();
 userRoutes
 .get('/info',verifyToken,tryCatch(getUserById))
 .get('/list',verifyToken,tryCatch(getProjectList))
+.get('/project/count',verifyToken,tryCatch(getUserProjectCount))
 
 export default userRoutes;
