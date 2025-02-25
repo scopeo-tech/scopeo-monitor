9c3c98b681c8743d2fe5a4ba493598f34e7b2217
Fathima Harsha U <fathimaharshaummatt@gmail.com>
2025-02-25 09:42:34 +0530

    Harsha home (#56)
    * feat: implement QueryProvider and Sidebar component for improved data handling and user interface
    
    * Merge branches 'harsha-home' and 'beta' of https://github.com/scopeo-tech/scopeo-monitor into harsha-home
    
    * Refactor authentication and layout components; add QueryProvider and Sidebar
    
    * Refactor layout components; add Navbar and update Sidebar structure
    
    * Update Navbar spacing for improved layout consistency
    
    * Add project management features: create Project interface, implement project store, and integrate project fetching in DefaultPage
    
    * Add project creation modal and user project count feature
    
    * Add date display and user information to project creation modal
    
    * Refactor Sidebar component for improved user information display and styling
    
    * Fix error type in project creation modal to use 'unknown' instead of 'any'

--- client/src/components/layout/sidebar/defaultSidebar.tsx
+++ client/src/components/layout/sidebar/defaultSidebar.tsx
@@ -1,13 +1,10 @@
 "use client"
 
-
 import { useQuery } from "@tanstack/react-query";
-import { getUserInfo, logoutUser ,getUserProjectCount} from "@/lib/api";
+import { getUserInfo, logoutUser, getUserProjectCount } from "@/lib/api";
 import { FC } from "react";
 import { useRouter } from "next/navigation";
 import { FaCog, FaQuestionCircle, FaSignOutAlt } from "react-icons/fa";
-
-
 
 const Sidebar: FC = () => {
   const router = useRouter();
@@ -29,50 +26,55 @@ const Sidebar: FC = () => {
         window.location.reload()
     }
 
-    if(isLoading||countLoading) return <div>Loading...</div>
-    if(isError||countError) return <div>Error</div>
 
-    return (
-        <div className="w-64 bg-green-400 text-white h-[calc(100vh-64px)] p-4 fixed left-0 top-16 shadow-lg">
+  if(isLoading||countLoading) return <div>Loading...</div>
+  if(isError||countError) return <div>Error</div>
+
+  return (
+    <div className="w-64 bg-emerald-400 text-white h-[calc(100vh-64px)] p-4 fixed left-0 top-16">
       <div className="text-center">
-        
-        <button className="text-sm text-white hover:underline flex items-center justify-center">
-          Create new project ✏
+        <button className="text-sm text-white hover:underline flex items-center justify-center gap-1">
+          Create new project
+          <span className="text-xs">⎇</span>
         </button>
       </div>
 
       {user && (
-        <div className="mt-6 p-3 bg-white text-black rounded-lg shadow-md">
-          <div className="flex items-center space-x-3">
-            <div className="w-10 h-10 bg-pink-500 text-white flex items-center justify-center rounded-full text-lg font-bold">
-              {user?.username[0].toUpperCase()}
+        <div className="mt-6  p-4 rounded-lg">
+          <div className="flex items-center gap-3">
+            <div className="w-8 h-8 bg-rose-500 text-white flex items-center justify-center rounded-full text-lg">
+             {user?.username[0].toUpperCase()} 
             </div>
             <div>
-              <h2 className="text-lg font-semibold">{user?user.username:"user Name"}</h2>
-              <p className="text-sm text-gray-600">{user.email}</p>
+              <h2 className="text-sm font-normal">{user?.username}</h2>
+              <p className="text-xs opacity-80">{user.email}</p>
             </div>
           </div>
-          <p className="mt-3 text-sm">
-            <strong>Joined on:</strong> {user?user.joinedDate:"Joined Date"}
-          </p>
-          <p className="text-sm">
-            <strong>Total Projects:</strong> {projectCount ?? "0"}
-          </p>
+          <div className="mt-4 space-y-1 text-sm">
+            <p className="flex gap-2">
+              <span className="opacity-80">Joined on</span>
+              <span>: {user?.joinedDate}</span>
+            </p>
+            <p className="flex gap-2">
+              <span className="opacity-80">Total Projects</span>
+              <span>: {projectCount ?? "0"}</span>
+            </p>
+          </div>
         </div>
       )}
 
-      <div className="mt-8 space-y-4">
-        <button className="flex items-center space-x-2 text-white hover:text-gray-200">
+      <div className="mt-48 space-y-4 p-4">
+        <button className="flex items-center space-x-2 text-white hover:text-white/80">
           <FaCog />
           <span>Settings</span>
         </button>
-        <button className="flex items-center space-x-2 text-white hover:text-gray-200">
+        <button className="flex items-center space-x-2 text-white hover:text-white/80">
           <FaQuestionCircle />
           <span>Get Help</span>
         </button>
         <button
           onClick={handleLogout}
-          className="flex items-center space-x-2 text-white hover:text-gray-200"
+          className="flex items-center space-x-2 text-white hover:text-white/80"
         >
           <FaSignOutAlt />
           <span>Logout</span>
@@ -80,6 +82,6 @@ const Sidebar: FC = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Sidebar
+export default Sidebar;
