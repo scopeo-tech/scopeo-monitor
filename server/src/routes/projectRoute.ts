e6d3bdee44eeca535e4bb9858094178129f0c76a
Navaf <navafsoft@gmail.com>
2025-03-01 15:16:23 +0530

    Navaf error monitor (#81)
    * add error tracking functionality and update error model
    
    * Merge branch 'beta' of https://github.com/scopeo-tech/scopeo-monitor into navaf-error-monitor
    
    * add error statistics endpoint and update types
    
    * Enhance error tracking and statistics: add methods for common errors, latest errors, and error method percentages; implement error count limit and cleanup.
    
    * Add error tracking endpoints: implement methods for common errors, latest errors, and error method percentages

--- server/src/routes/projectRoute.ts
+++ server/src/routes/projectRoute.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { checkProjectName, createProject ,getApiKey, getPassKey, getProjectPassKey,updateProject,deleteProject, getErrorStats} from "../controller/project/projectController";
 import verifyToken from "../middleware/verifyToken";
 import tryCatch from "../lib/util/tryCatch";
+import { getCommonError, getErrorMethodPercentages, getLatestError } from "../controller/agent/errorTrack";
 
 
 const router = express.Router();
@@ -18,6 +19,11 @@ router
 .put("/update-project/:projectId",tryCatch(updateProject))
 .post("/check-project-name",tryCatch(checkProjectName))
 .delete("/delete-project/:projectId",tryCatch(deleteProject))
-.get("/error-stats",tryCatch(getErrorStats))
+
+//Error Stats
+.get("/get-error-stats/:projectId",tryCatch(getErrorStats))
+.get("/get-error-common/:projectId",tryCatch(getCommonError))
+.get("/get-error-latest/:projectId",tryCatch(getLatestError))
+.get("/get-error-method/:projectId",tryCatch(getErrorMethodPercentages))
 
 export default router
