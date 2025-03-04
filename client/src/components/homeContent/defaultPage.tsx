107c5baa8a6cae83aa28b973492b38f86d60e88c
harsha <fathimaharshaummatt@gmail.com>
2025-03-04 14:12:05 +0530

    Refactor navigation in components to use Link from next/link for improved routing

--- client/src/components/homeContent/defaultPage.tsx
+++ client/src/components/homeContent/defaultPage.tsx
@@ -8,7 +8,7 @@ import { Project } from "@/lib/interface";
 import CreateProjectModal from "../modal/createProjectModal";
 import { FiEdit, FiEye, FiEyeOff } from "react-icons/fi";
 import { FaCopy } from "react-icons/fa";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const DefaultPage: FC = () => {
   const [formattedDate, setFormattedDate] = useState<string>("");
@@ -18,7 +18,6 @@ const DefaultPage: FC = () => {
   const [copiedApiKey, setCopiedApiKey] = useState<string | null>(null);
   const [copiedPassKey, setCopiedPassKey] = useState<string | null>(null);
   const { user } = useAuthStore();
-  const router = useRouter();
 
   const { data: projects, isLoading, isError } = useQuery<Project[]>({
     queryKey: ["userProjects"],
@@ -133,8 +132,8 @@ const DefaultPage: FC = () => {
               {Array.isArray(projects) &&
                 projects.map((project: Project) => (
                   <tr key={project._id} className="border-b text-sm hover:bg-gray-50">
-                    <td className="py-4 px-4 text-gray-700 cursor-pointer" 
-                      onClick={() => router.push(`/${project._id}/error`)}>{project.name}</td>
+                    <Link  href={`/${project._id}/error`} className="py-4 px-4 text-gray-700 cursor-pointer">
+                          {project.name}</Link>
                     <td className="py-4 px-4 ml-8">
                       <span
                         className={`inline-block w-2 h-2 rounded-full hover: ${project.status.connectionStatus
