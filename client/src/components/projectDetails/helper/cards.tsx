5b69cca1f594d2d4b3d3db47526bcfa6feefd00f
MohammedIrfan244 <kuttappuirfan244@gmail.com>
2025-03-05 11:18:07 +0530

    Fix spacing in "Total attempts" label in project details cards

--- client/src/components/projectDetails/helper/cards.tsx
+++ client/src/components/projectDetails/helper/cards.tsx
@@ -21,7 +21,7 @@ const Cards = ({ stats = { totalLogins: 0, successLogins: 0, failedLogins: 0, to
                 <div>
                     <p className='text-gray-400 font-semibold text-sm'>Total logins</p>
                     {isLoading ? <p className='text-[#16C47F] text-3xl font-bold'>00</p> : <p className='text-[#16C47F] text-3xl font-bold'>{stats.totalLogins}</p>}
-                    <p className='text-[#16C47F] text-xs'>Total attempts</p>
+                    <p className='text-[#16C47F] text-xs'>Total attempts </p>
                 </div>
                 <div className='h-full flex items-center flex-grow justify-center'>
                     <AiOutlineCheckCircle className='text-[#16C47F] text-5xl' />
