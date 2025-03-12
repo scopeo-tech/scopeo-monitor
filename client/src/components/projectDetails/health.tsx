18f8e2ac995b0f0c19af9c2e2de2a6498a7fc7d0
Mohammed Irfan <kuttappuirfan244@gmail.com>
2025-03-12 07:15:12 +0530

    Irfan perfomance client (#129)
    * feat: enhance Health component layout with HealthGraph and grid structure
    
    * feat: add performance metrics and health metrics interfaces, API endpoints, and HealthGraph component

--- client/src/components/projectDetails/health.tsx
+++ client/src/components/projectDetails/health.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import HealthGraph from './helper/healthGraph'
+
 
 const Health = () => {
   return (
-    <div>Health</div>
+    <div className="grid grid-cols-3 gap-4 p-4">
+      {/* First row */}
+      <div className="bg-gray-200 p-4 col-span-1">
+      </div>
+      <div className="bg-gray-300 p-4 col-span-2">
+        <HealthGraph />
+      </div>
+
+      {/* Second row */}
+      <div className="bg-gray-400 p-4 col-span-1">Part 3</div>
+      <div className="bg-gray-500 p-4 col-span-1">Part 4</div>
+      <div className="bg-gray-600 p-4 col-span-1">Part 5</div>
+    </div>
   )
 }
 
