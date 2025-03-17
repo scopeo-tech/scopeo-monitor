cea552c97ca39534c42038e422dd362f4aa74c5e
Navaf <navafsoft@gmail.com>
2025-03-17 16:33:22 +0530

    Navaf faq (#142)
    * feat: add FAQ component and page for user assistance
    
    * feat: add FAQ layout and page components with navigation
    
    * feat: enhance FAQ component with search functionality and improved layout
    
    * feat: update FAQ model to include unique questions, keywords, and search count
    
    * feat: add FAQ routes and controller for handling FAQ requests
    
    * feat: implement endpoint to retrieve top FAQs based on search count
    
    * feat: add API endpoint to retrieve top FAQs and define FAQ interface
    
    * feat: enhance FAQ component with dynamic loading and interaction for FAQs
    
    * feat: add animated question mark to FAQ header for enhanced interaction
    
    * refactor: remove unnecessary console logs and error handling in performance and cron job controllers
    
    * feat: add custom useDebounce hook for improved value handling
    
    * feat: add AI integration for FAQ suggestions and generation
    
    * feat: add search and AI generation endpoints for FAQs
    
    * feat: add search and AI generation functionalities for FAQs
    
    * feat: enhance FAQ component with AI suggestions and search functionality

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -302,5 +302,20 @@ export const contactUs = async (data:{firstname:string,lastname:string,email:str
 export const getTopFaqs = async () => {
   const response = await api.get("/faq/get-top-faqs");
   return response.data;
-};  
+};
 
+export const searchFaqs = async (query: string) => {
+  const response = await api.get(`/faq/get-search-faqs?query=${query}`);
+  return response.data;
+};
+
+export const getAISuggestion = async (query: string) => {
+  const response = await api.get(`/faq/get-ai-suggestion?query=${query}`);
+  return response.data.suggestion;
+};
+
+export const getAIGeneratedFaq = async (query: string) => {
+  const response = await api.get(`/faq/get-ai-generated-faqs?query=${query}`);
+  console.log("fronedn faq ai",response.data.data);
+  return response.data.data;
+};
--- server/src/routes/faqRoutes.ts
+++ server/src/routes/faqRoutes.ts
@@ -1,9 +1,12 @@
 import e from "express";
 import tryCatch from "../lib/util/tryCatch";
-import { getTopFaqs } from "../controller/faq/faqController";
+import { AiSuggestion, generateAIFAQs, getTopFAQs, searchFAQs } from "../controller/faq/faqController";
 const faqRouter = e.Router();
 
 faqRouter
-.get("/get-top-faqs", tryCatch(getTopFaqs));
+.get("/get-top-faqs", tryCatch(getTopFAQs))
+.get("/get-search-faqs", tryCatch(searchFAQs))
+.get("/get-ai-suggestion", tryCatch(AiSuggestion))
+.get("/get-ai-generated-faqs", tryCatch(generateAIFAQs))
 
 export default faqRouter
