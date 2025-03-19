eefc1e604a6e7b8a56ddb489e901a53cac4659bc
navaf <navafsoft@gmail.com>
2025-03-19 12:51:27 +0530

    Add resolved flag to error schema and implement AI error analysis endpoints

--- server/src/routes/projectRoute.ts
+++ server/src/routes/projectRoute.ts
@@ -3,7 +3,7 @@ import { checkProjectName, createProject ,getApiKey, getPassKey, getProjectPassK
 import verifyToken from "../middleware/verifyToken";
 import tryCatch from "../lib/util/tryCatch";
 import {getBruteForceAttempts,getFailedLogins,getTotalLogins,getUnusualLogins,getSecurityStats} from "../controller/agent/security"
-import { getErrorStats, getCommonError, getErrorMethodPercentages, getLatestError, getAllErrors } from "../controller/agent/errorTrack";
+import { getErrorStats, getCommonError, getErrorMethodPercentages, getLatestError, getAllErrors, getAiErrorAnalysis, resolveError, unResolveError } from "../controller/agent/errorTrack";
 import { getLogs } from "../controller/agent/logs";
 import { getErrorStabilityMetrics, getPerformanceData, getServerPerformanceMetrics, getSystemHealthMetrics, getTrafficLoadMetrics } from "../controller/agent/performance";
 import { getNotifications, markAsRead } from "../controller/project/notificationController";
@@ -30,6 +30,9 @@ router
 .get("/get-error-latest/:projectId",tryCatch(getLatestError))
 .get("/get-error-method/:projectId",tryCatch(getErrorMethodPercentages))
 .get("/get-all-errors/:projectId",tryCatch(getAllErrors))
+.post("/get-error-analysis",tryCatch(getAiErrorAnalysis))
+.post("/resolve-error/:id",tryCatch(resolveError))
+.post("/unresolve-error/:id",tryCatch(unResolveError))
 //Security Stats
 .get("/get-brute-force/:projectId",tryCatch(getBruteForceAttempts))
 .get("/get-failed-logins/:projectId",tryCatch(getFailedLogins))
