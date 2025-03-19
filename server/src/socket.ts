a3ea09f261b25b9822dd2adfce7dc1e106258a0b
Mohammed Irfan <kuttappuirfan244@gmail.com>
2025-03-19 08:58:47 +0530

    Irfan socket.io (#151)
    * feat: integrate Socket.IO for real-time notifications and add notification handling in security logs
    
    * feat: enhance notification handling with user registration and logging
    
    * fix: resolve merge conflict in notificationStore.ts
    
    * refactor: clean up socket handling and notification logic in client and server
    
    * feat: add notification handling and logging functionality in server and client

---
+++ server/src/socket.ts
@@ -0,0 +1,115 @@
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import Notification from "./model/notiModel";
+import { INotification } from "./model/notiModel";
+import express from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { ILog } from "./model/logModel";
+
+dotenv.config();
+
+interface UserSocketMap {
+  [userId: string]: string;
+}
+
+export const userSocketMap: UserSocketMap = {}; // stores the socket id of each user that is connected
+console.log(process.env.CLIENT_URL);
+export const app = express();
+export const server = createServer(app);
+export const io = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST"],
+  },
+});
+
+io.use((socket, next) => {
+  const pass = socket.handshake.auth.pass;
+  if (!pass) {
+    console.log("no token");
+    return next(new Error("No socket token!"));
+  }
+
+  try {
+    const decoded = jwt.verify(pass, process.env.JWT_TOKEN as string);
+    socket.handshake.auth.userId = (decoded as { id: string }).id;
+    socket.data.userId = (decoded as { id: string }).id;
+    console.log("decoded", decoded);
+  } catch (error) {
+    console.log("socket pass decode error", error);
+    return next(new Error("Invalid socket token!"));
+  }
+
+  next();
+});
+
+io.on("connection", async (socket) => {
+  console.log("a user connected", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
+  });
+
+  // user join event
+  socket.on("join", () => {
+    console.log("user joined", socket.data.userId);
+    userSocketMap[socket.data.userId] = socket.id;
+    console.log("user joined", socket.data.userId);
+    console.log(userSocketMap);
+  });
+
+  //send notification event
+  socket.on(
+    "notification",
+    async (notification : INotification) => {
+      const receiverSocketId = userSocketMap[notification.user.toString() as string];
+
+      //create notificaion in db
+      try {
+        // send notification to receiver
+        if (receiverSocketId) {
+          socket.to(receiverSocketId).emit("notification", notification);
+        }
+      } catch (error) {
+        console.log("Error creating notification:", error);
+        console.log("Notification data:", notification);
+      }
+    }
+  );
+
+  // logs event handler
+socket.on(
+  "logs",
+  async (logData: ILog,userId:string) => {
+    try {
+      const receiverSocketId = userSocketMap[userId];
+      
+      if (receiverSocketId) {
+        // Emitting to the same user who triggered the security event
+        socket.to(receiverSocketId).emit("logs", logData);
+      }
+      
+      // Optional: You could also log security events or perform additional actions
+      console.log("Log event received:", {
+       ...logData,
+      });
+      
+    } catch (error) {
+      console.log("Error processing security event:", error);
+      console.log("Security data:", logData);
+    }
+  }
+);
+
+  socket.on("disconnect", () => {
+    const disconnectedUser = Object.keys(userSocketMap).find(
+      (key) => userSocketMap[key] === socket.id
+    );
+    if (disconnectedUser) {
+      delete userSocketMap[disconnectedUser];
+    }
+    console.log("A user disconnected:", socket.id);
+    console.log(userSocketMap);
+  });
+});
