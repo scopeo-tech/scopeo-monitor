ecf6d01dc21b673d4d797d6e4cd04a7e0c307d32
Navaf <navafsoft@gmail.com>
2025-03-24 15:38:49 +0530

    Navaf doc (#182)
    * refactor: disable exhaustive-deps eslint rule and update sidebar settings path
    
    * Merge branch 'beta' of https://github.com/scopeo-tech/scopeo-monitor into navaf-doc
    
    * feat: add DocConfig and DocText components for documentation
    
    * feat: refactor token handling and enhance SkeletonBox component with optional className
    
    * Merge branch 'beta' of https://github.com/scopeo-tech/scopeo-monitor into navaf-doc
    
    * feat: restructure navbar components and implement authentication wrapper for protected routes
    
    * feat: update navbar links to point to correct routes for Developers, Documentation, and About Us
    
    * refactor: remove FAQ layout and page components; add express-rate-limit dependency
    
    * feat: add FAQ page component to navbar
    
    * refactor: rename FAQ component function to 'page' for consistency
    
    * feat: add Radix UI tabs dependency and Tailwind CSS animation plugin; update global styles
    
    * feat: add Alert and Tabs components; implement utility function for class name merging
    
    * feat: remove DocConfig and DocText components; add new page component for documentation
    
    * Merge branch 'beta' of https://github.com/scopeo-tech/scopeo-monitor into navaf-doc
    
    * Merge branch 'beta' of https://github.com/scopeo-tech/scopeo-monitor into navaf-doc
    
    * Add Redis configuration and caching middleware
    
    * Add caching middleware to FAQ, Project, and User routes
    
    * Add rate limiting middleware using Redis
    
    * Merge branch 'beta' of https://github.com/scopeo-tech/scopeo-monitor into navaf-doc
    
    * Refactor Navbar links and improve Redis connection logging
    
    * Remove unnecessary console.log statements from authentication and notification components
    
    * Add NotFound component and enhance AIAnalysisModal styling and structure

--- server/src/config/redisConfig.ts
+++ server/src/config/redisConfig.ts
@@ -4,6 +4,6 @@ dotenv.config();
 const redisClient = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
 
 redisClient.on("connect", () => console.log("Redis connected via Docker"));
-redisClient.on("error", (err) => console.error(" Redis error", err));
+redisClient.on("error", (err) => console.error("Redis error", err));
 
 export default redisClient;
