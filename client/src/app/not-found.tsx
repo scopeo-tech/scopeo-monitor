a69aadf9202ae8485aea61f3394eabc405dc0d03
harsha <fathimaharshaummatt@gmail.com>
2025-03-26 12:38:47 +0530

    Enhance Not Found page with close button and update footer links for better navigation

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -1,25 +1,28 @@
 import Image from "next/image";
 import Link from "next/link";
 import notfound from "@/assets/notfound.svg";
+import { FaTimes } from "react-icons/fa";
 
 const NotFound = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen  text-white text-center px-6">
-      <Image src={notfound} alt="Not Found" width={400} height={400} />
+    <div className="relative flex flex-col items-center justify-center h-screen text-white text-center px-6">
+      {/* Close Button */}
+      <Link href="/" className="absolute top-5 right-5 text-gray-400  text-2xl">
+        <FaTimes />
+      </Link>
+
+      <Image src={notfound} alt="Not Found" width={500} height={500} />
 
       {/* Error Message */}
-      <h1 className="text-5xl font-bold mt-[-20px] text-emerald-500">Oops! Page Not Found</h1>
-<p className="text-lg text-gray-400 mt-[10px]">
-  The page you&apos;re looking for doesn&apos;t exist or has been moved.
-</p>
-
-
-      {/* Back to Home Button */}
-      <Link href="/" className="mt-6 px-6 py-3 bg-emerald-500 text-white rounded-lg shadow-lg hover:bg-emerald-600 transition">
-        Go Back Home
-      </Link>
+      <h1 className="text-5xl font-semibold mt-[-20px] text-emerald-500">
+        Oops! Page Not Found
+      </h1>
+      <p className="text-lg text-gray-400 mt-[10px]">
+        The page you&apos;re looking for doesn&apos;t exist or has been moved.
+      </p>
     </div>
   );
 };
 
 export default NotFound;
+
