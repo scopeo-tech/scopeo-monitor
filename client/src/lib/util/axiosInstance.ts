33537ba10fa4257778d09889f47056640b7b9d03
Mohammed Irfan <kuttappuirfan244@gmail.com>
2025-03-26 12:24:52 +0530

    Irfan pass sec (#191)
    * update README and axios instance for production URL
    
    * Add PDF download functionality and update project data retrieval in various components

--- client/src/lib/util/axiosInstance.ts
+++ client/src/lib/util/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import axiosErrorManager from "./axiosErrorManager";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 });
 
