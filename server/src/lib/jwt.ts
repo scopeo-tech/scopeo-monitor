558497df91d931ede77e13adfe7f89aef07fed06
navaf <navafsoft@gmail.com>
2025-03-28 10:36:22 +0530

    Remove refresh token functionality from auth controller and routes

--- server/src/lib/jwt.ts
+++ server/src/lib/jwt.ts
@@ -3,7 +3,3 @@ import jwt from "jsonwebtoken";
 export const createAccessToken = (userId: string, secret: string) => {
   return jwt.sign({ userId }, secret, { expiresIn: "7d" });
 };
-
-export const createRefreshToken = (userId: string, secret: string) => {
-  return jwt.sign({ userId }, secret, { expiresIn: "7d" });
-};
