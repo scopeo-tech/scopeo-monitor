9e185acc0d91e9e643a674f8cced17d6fa2ac4f7
MohammedIrfan244 <kuttappuirfan244@gmail.com>
2025-04-02 15:59:35 +0530

    Fix typo in "Who Are We" section heading to "Who We Are"

--- client/src/components/navbar/about.tsx
+++ client/src/components/navbar/about.tsx
@@ -75,7 +75,7 @@ const About: React.FC = () => {
 
       {/* Who Are We Section */}
       <div className="py-20 text-center bg-white">
-      <h2 className="text-3xl font-bold mb-10">Who Are We</h2>
+      <h2 className="text-3xl font-bold mb-10">Who We Are</h2>
 
       <div className="flex flex-col md:flex-row justify-around gap-8 px-4 md:px-10">
         {[
